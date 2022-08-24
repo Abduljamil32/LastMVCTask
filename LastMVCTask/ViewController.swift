888909d0b0c09c64541afbbd04bd9ee56bc53c7b
co.abu <anizomxoja@gmail.com>
2022-08-24 17:12:04 +0500

    Initial Commit

---
+++ LastMVCTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LastMVCTask
+//
+//  Created by Avaz Mukhitdinov on 24/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
